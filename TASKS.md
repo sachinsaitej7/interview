72f00e3182eddb996c755e1e94e3c4ce342a2df8
Nageswara <nageswara@syftdata.com>
2024-04-25 12:59:54 -0700

    more tasks

--- TASKS.md
+++ TASKS.md
@@ -1,8 +1,18 @@
-1. Make the front page look pretty.
-   1. get caraousel working.
-   3. Make Plot show up on hover. (extra points: after 500ms delay)
-   4. Show a fallback image when image fails to load.
-2. Make the backend logic better.
-   1. sort genre list.
-   2. Rotten Tomators (provider rt) are not showing up right.
-   3. expose functionality to favorite a movie. (keep favorites in memory)
+### Bugs
+1. Rotten Tomators (provider rt) are not showing up right.
+   RT and Imdb differences:
+   1. field names might be different.
+   2. Poster in RT comes as a first image in images.
+2. Genre List is not sorted. 
+3. Show a fallback image when image fails to load.
+
+### Tasks
+   1. Favorite feature.
+      1. [Backend] Expose functionality to favorite a movie. (keep favorites in memory)
+      2. [Frontend] Show "star" icon to enable favorite.
+   2. Make the front page look pretty.
+      1. Get caraousel working. https://preline.co/docs/carousel.html     
+      2. Make Plot show up on hover. (extra points: after 500ms delay)
+      3. Make thumbnails switch from time to time.
+
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,83 +1,9 @@
-import Link from "next/link";
-import { Camera } from 'lucide-react';
 import RecentMovies from "./movies/recent-movies";
 import GenreList from "./movies/genre-list";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-6 items-center p-12">
-      <div className="flex flex-row z-10 max-w-5xl w-full items-center text-sm lg:flex">
-        <Camera
-            alt="Movie Logo"
-            className="dark:invert"
-            width={100}
-            height={24}
-        />
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Syft Movies
-        </p>
-      </div>
-      <div className="grid text-center lg:max-w-5xl lg:w-full lg:grid-cols-4 lg:text-left">
-        <Link
-          href="/movies"
-          className="group rounded-lg border border-transparent px-3 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Movies{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Browse our extensive movie catalog.
-          </p>
-        </Link>
-
-        <Link
-          href="/genres"
-          className="group rounded-lg border border-transparent px-3 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Genres{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore movies by genre.
-          </p>
-        </Link>
-
-        <Link
-          href="/new-releases"
-          className="group rounded-lg border border-transparent px-3 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            New Releases{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Check out the latest movies added to our catalog.
-          </p>
-        </Link>
-
-        <Link
-          href="/top-rated"
-          className="group rounded-lg border border-transparent px-3 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Top Rated{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            See the highest rated movies in our catalog.
-          </p>
-        </Link>
-      </div>      
       <RecentMovies />
       <GenreList />
     </main>
